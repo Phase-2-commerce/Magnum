4a1e6d8364e8cfeda961a277708a431c38076a90
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-08-13 11:37:10 +0300

    module update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 // tailwind.config.js
-module.exports = {
-	theme: {
-		extend: {
-			colors: {
-				primary: {
-					orange: "#DB4444", 
-				},
-				secondary: {
-					green: "#00FF66", 
-				},
-			},
-		},
-	},
-	plugins: [],
+export const theme = {
+  extend: {
+    colors: {
+      primary: {
+        orange: "#DB4444",
+      },
+      secondary: {
+        green: "#00FF66",
+      },
+    },
+  },
 };
+export const plugins = [];
 
 
