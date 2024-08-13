081ba922e5cb237ccd5cd393fe51e1eb757bc212
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-13 15:30:47 +0300

    Finished The NavBar with TailWind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,25 +3,26 @@ import '../NavBar.css'
 import { IoSearchOutline } from "react-icons/io5";
 import { IoHeartOutline } from "react-icons/io5";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+
 const NavBar = () => {
   return (
     <>
-     <nav>
-        <div className="main-container">
-            <div className="name">
-                <p className='home-header'>Exclusive</p>
-                <ul>
-                    <li>Home</li>
-                    <li>Contact</li>
-                    <li>About</li>
-                    <li>Sign Up</li>
+     <nav className = "h-[100px] border-b border-gray-500">
+        <div className = "flex justify-around w-[1440px] h-[38px] relative top-[50px] left-[135px] gap-[148px]">
+            <div className = "flex gap-[190px] items-center">
+                <p className='font-bold text-2xl leading-6 tracking-wide'>Exclusive</p>
+                <ul className = "list-none flex gap-[48px]">
+                    <li className="font-poppins font-normal text-base leading-6 text-center">Home</li>
+                    <li className="font-poppins font-normal text-base leading-6 text-center">Contact</li>
+                    <li className="font-poppins font-normal text-base leading-6 text-center">About</li>
+                    <li className="font-poppins font-normal text-base leading-6 text-center">Sign Up</li>
                 </ul>
             </div>
-            <div className="search-container">
-                <input type="text" placeholder="What are you looking for?" />
-                <IoSearchOutline className='icon' />
-                <IoHeartOutline className='heart-icon' />
-                <AiOutlineShoppingCart  className='cart-icon'/>
+            <div className="flex w-[347px] h-[24px] gap-[24px] items-center">
+                <input type="text" placeholder="What are you looking for?" className="font-poppins font-normal text-xs leading-[18px] w-[243px] h-[38px] opacity-50 bg-gray-300 p-2 rounded-md"/>
+                <IoSearchOutline className="w-[24px] h-[24px] -ml-[60px]" src="icon.png" alt="Icon" />
+                <IoHeartOutline className="w-[24px] h-[24px] text-center" src="heart-icon.png" alt="Heart Icon" />
+                <AiOutlineShoppingCart  className="w-[24px] h-[24px] text-center" src="cart-icon.png" alt="Cart Icon"/>
             </div>
         </div>
      </nav>
