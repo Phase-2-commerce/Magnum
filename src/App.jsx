467142440bf60c480a93783df92826282f0571a2
KiniBrown76 <esther.kini@student.moringaschool.com>
2024-08-14 13:24:37 +0300

    login and sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import NavBar from "./components/NavBar"
 import ProductDetails from './components/ProductDetails'
+import SignupForm from './components/SignUpForm'
+import LoginForm from './components/LoginForm'
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
     <>
      <NavBar />
      <ProductDetails/>
+     <SignupForm/>
+     <LoginForm/>
     </>
   )
 }
