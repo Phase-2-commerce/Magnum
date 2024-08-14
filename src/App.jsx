d43c2505f2593ee36c42a3c520eaa1f9e13af2a9
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-14 10:59:48 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 // import React from 'react'
-import About from "./components/About"
-// import Footer from "./components/Footer"
-// import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+import NavBar from "./components/NavBar"
 
 const App = () => {
   return (
     <>
-     {/* <NavBar />
-     <Footer /> */}
-     <About />
+     <NavBar />
+     <Footer />
     </>
   )
 }
