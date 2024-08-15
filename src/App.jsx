b214fae764e5f766619f22df25775f416d8a2409
Abdulmajid Aman Hussein <abdulmajid.aman.student@moringaschool.com>
2024-08-15 19:19:04 +0300

    "Uncommented Footer import and component in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // import React from 'react'
 import NavBar from "./components/NavBar"
-// import Footer from './components/Footer'
+import Footer from './components/Footer'
 
 function App() {
   return (
     
     <>
      <NavBar />
-     {/* <Footer/> */}
+     <Footer/>
 
     </>
   )
